b3df90130d2053e26aeb766bb8fd666014f06e70
Francisco Sancho <frsancho1985@gmail.com>
2024-12-19 10:25:01 -0300

    implementing services

--- 03_rest-api-spring-boot-dependency-injection/src/main/java/br/com/sancho/rest_api_spring_boot/controllers/PersonController.java
+++ 03_rest-api-spring-boot-dependency-injection/src/main/java/br/com/sancho/rest_api_spring_boot/controllers/PersonController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -18,6 +20,11 @@ public class PersonController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) {
         return services.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return services.findAll();
     }
 
 
