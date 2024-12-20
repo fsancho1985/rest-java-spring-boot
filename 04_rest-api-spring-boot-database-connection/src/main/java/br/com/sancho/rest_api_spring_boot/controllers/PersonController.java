73afd2e2519e1f3d48aa6133231caaef334ea77c
Francisco Sancho <frsancho1985@gmail.com>
2024-12-20 10:28:54 -0300

    Implementing DataBase Connection

---
+++ 04_rest-api-spring-boot-database-connection/src/main/java/br/com/sancho/rest_api_spring_boot/controllers/PersonController.java
@@ -0,0 +1,49 @@
+package br.com.sancho.rest_api_spring_boot.controllers;
+
+
+import br.com.sancho.rest_api_spring_boot.model.Person;
+import br.com.sancho.rest_api_spring_boot.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices services;
+//    private PersonServices services = new PersonServices();
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") Long id) {
+        return services.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return services.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return services.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return services.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") Long id) {
+        services.delete(id);
+    }
+    
+}
