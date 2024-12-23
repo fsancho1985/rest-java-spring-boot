f4feb5b5aec5f37f2338687c40b6a2e093143a87
Francisco Sancho <frsancho1985@gmail.com>
2024-12-23 10:30:55 -0300

    Removing unused annotations

--- 04_rest-api-spring-boot-database-connection/src/main/java/br/com/sancho/rest_api_spring_boot/repositories/PersonRepository.java
+++ 04_rest-api-spring-boot-database-connection/src/main/java/br/com/sancho/rest_api_spring_boot/repositories/PersonRepository.java
@@ -4,6 +4,6 @@ import br.com.sancho.rest_api_spring_boot.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
