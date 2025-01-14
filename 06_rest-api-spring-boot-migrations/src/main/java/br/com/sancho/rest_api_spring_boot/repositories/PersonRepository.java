7ef0aed21f5ff8b5ed50eb69000c393faef2ac27
Francisco Sancho <frsancho1985@gmail.com>
2025-01-14 10:32:41 -0300

    Add migrations to project

---
+++ 06_rest-api-spring-boot-migrations/src/main/java/br/com/sancho/rest_api_spring_boot/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.sancho.rest_api_spring_boot.repositories;
+
+import br.com.sancho.rest_api_spring_boot.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
---
+++ 06_rest-api-spring-boot-migrations/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+(1, 'Francisco de Souza', 'Sancho', 'Santo André, São Paulo - Brasil', 'Male'),
+(2, 'Julcenia Alcantara', 'Sancho', 'Camaçari, Bahia - Brasil', 'Female'),
+(3, 'Elizeu', 'Nunes', 'São Paulo, São Paulo - Brasil', 'Male'),
+(4, 'Sheylla', 'Nunes', 'São Paulo, São Paulo - Brasil', 'Female');
